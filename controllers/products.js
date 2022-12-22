a827fc47ad0c0c4cf777cc03d2ea8c61eca2c79c
Abiodun Adefila <adefaze@gmail.com>
2022-12-22 13:42:52 +0100

    pagination and limit

--- controllers/products.js
+++ controllers/products.js
@@ -50,6 +50,14 @@ const getAllProducts = async (req, res) => {
         results = results.select(fieldsList)
     }
 
+    // limit and pagination
+
+    const limit = Number(req.query.limit) || 10
+    const page = Number(req.query.page) || 1
+    const skip = (page - 1) * limit
+
+    results = results.skip(skip).limit(limit)
+
     const products = await results
     res.status(200).json({ products, nbHits: products.length })
 }
